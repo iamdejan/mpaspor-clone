83249934650b435e7c820791117023c91b40b96f
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2025-02-16 14:25:06 +0700

    feat(workflow): list by creator

--- app/Http/Controllers/PassportController.php
+++ app/Http/Controllers/PassportController.php
@@ -8,19 +8,49 @@ use App\Workflows\ApplyPassportWorkflow;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Redirect;
 use App\Models\PassportApplication;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class PassportController extends Controller
 {
+    public function index(Request $request): Response {
+        $list = PassportApplication::where('created_by', $request->user()->id)->orderBy('created_at')->get();
+
+        $applications = [];
+        foreach ($list as $entry) {
+            $workflow = WorkflowStub::load($entry->workflow_id);
+            array_push($applications, [
+                "workflow_id" => $workflow->id(),
+                "status" => $this->mapStatus($workflow->status()),
+            ]);
+        }
+
+        return Inertia::render('Dashboard')->with(["passport_applications" => $applications]);
+    }
+
     public function createApplyRequest(Request $request): RedirectResponse {
         $workflow = WorkflowStub::make(ApplyPassportWorkflow::class);
         $workflow->start();
         $workflow->complete();
 
-        $passportApplication = PassportApplication::create([
+        PassportApplication::create([
             "workflow_id" => $workflow->id(),
             "created_by" => $request->user()->id
         ]);
 
         return Redirect::route('dashboard');
+    }
+
+    private static $status_map = [
+        "Workflow\States\WorkflowFailedStatus" => "failed",
+        "Workflow\States\WorkflowCompletedStatus" => "completed",
+        "Workflow\States\WorkflowCreatedStatus" => "created",
+        "Workflow\States\WorkflowPendingStatus" => "pending",
+        "Workflow\States\WorkflowRunningStatus" => "running",
+        "Workflow\States\WorkflowWaitingStatus" => "waiting",
+    ];
+
+    private function mapStatus(string $status): string {
+        return static::$status_map[$status];
     }
 }
