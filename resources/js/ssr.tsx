ec0fdcd9d2dbc554bc2fac275f5b703cbb9b2d5e
Giovanni Dejan <15686093+iamdejan@users.noreply.github.com>
2025-02-24 15:23:07 +0700

    fix: tsc error on newly setup computer (#7)

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -4,6 +4,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import ReactDOMServer from 'react-dom/server';
 import { RouteName } from 'ziggy-js';
 import { route } from '../../vendor/tightenco/ziggy';
+import { PageProps } from './types';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -22,8 +23,8 @@ createServer((page) =>
             // @ts-expect-error
             global.route<RouteName> = (name, params, absolute) =>
                 route(name, params as any, absolute, {
-                    ...page.props.ziggy,
-                    location: new URL(page.props.ziggy.location),
+                    ...(page.props.ziggy as PageProps['ziggy']),
+                    location: new URL((page.props.ziggy as PageProps['ziggy']).url || (page.props.ziggy as PageProps['ziggy']).location),
                 });
             /* eslint-enable */
 
